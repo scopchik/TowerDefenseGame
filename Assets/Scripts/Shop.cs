77c84176a60b2b2272e161427c3cde36be4927a3
Evgeny Skopin <79599538+scopchik@users.noreply.github.com>
2024-05-24 19:48:21 +0300

    Update Shop.cs

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -2,32 +2,33 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    BuildManager buildManager;
+    
 
     public TurretBlueprint standartTurret;
     public TurretBlueprint missileLauncher;
     public TurretBlueprint laserBeamer;
-
+    BuildManager buildManager;
+    
     void Start()
     {
         buildManager = BuildManager.instance;
     }
+    
     public void SelectStandartTurret()
     {
         Debug.Log("Standart Turret Pusrchased");
-        buildManager.SelectTurretToBuild(standartTurret);
-        
+        buildManager.SelectTurretToBuild(standartTurret);   
     }
+    
     public void SelectMissileLauncher()
     {
         Debug.Log("Missile Launcher Pusrchased");
         buildManager.SelectTurretToBuild(missileLauncher);
-        
     }
+    
     public void SelectLaserBeamer()
     {
         Debug.Log("Laser Beamer Pusrchased");
-        buildManager.SelectTurretToBuild(laserBeamer);
-        
+        buildManager.SelectTurretToBuild(laserBeamer);   
     }
 }
